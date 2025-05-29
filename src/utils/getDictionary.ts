22bb677da2c13b947a474c01b7337b86c3ae6145
Mohammed BEN SAID <mohammed.ben.said@outlook.fr>
2025-05-29 22:46:55 +0200

    Update translations in Arabic, English, and French locales: Enhance patient and invoice forms with new keys for improved clarity and consistency. Refactor dictionary imports in getDictionary utility for better organization and maintainability.

--- src/utils/getDictionary.ts
+++ src/utils/getDictionary.ts
@@ -8,6 +8,13 @@ const dictionaries = {
   en: () => import('@/locales/en.json').then(module => module.default),
   fr: () => import('@/locales/fr.json').then(module => module.default),
   ar: () => import('@/locales/ar.json').then(module => module.default)
+
+  /*
+  en: () => import('@/data/dictionaries/en.json').then(module => module.default),
+  fr: () => import('@/data/dictionaries/fr.json').then(module => module.default),
+  ar: () => import('@/data/dictionaries/ar.json').then(module => module.default)
+
+  */
 }
 
 export const getDictionary = async (locale: Locale) => dictionaries[locale]()
