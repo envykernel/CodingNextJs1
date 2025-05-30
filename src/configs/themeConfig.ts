cb77817de6ebf8493083222dc92d81b1c23669f1
Mohammed BEN SAID <mohammed.ben.said@outlook.fr>
2025-05-31 01:29:31 +0200

    Update theme configuration: Change template name from 'Vuexy' to 'MedSky' to reflect new branding.

--- src/configs/themeConfig.ts
+++ src/configs/themeConfig.ts
@@ -54,7 +54,7 @@ export type Config = {
 }
 
 const themeConfig: Config = {
-  templateName: 'Vuexy',
+  templateName: 'MedSky',
   homePageUrl: '/dashboards/organization',
   settingsCookieName: 'vuexy-mui-next-demo-1',
   mode: 'system', // 'system', 'light', 'dark'
